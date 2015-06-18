924ceee8a15985bfbd9954d2edd3a80a0b6e17b5
Jacob Arriola <jacob.arriola@gmail.com>
2015-06-17 21:05:40 -0700

    Fix social paths and add happy hour time

--- footer.php
+++ footer.php
@@ -7,10 +7,10 @@
 			<div class="grid_5 general-info">
 
 				<p>1400 Sunset Blvd. Echo Park, CA 90025<br><br>
-				<span>213-482-9209</span>  |   <span><a href="mailto:info@thepark1400sunset.com">info@thepark1400sunset.com</a></span></p>
+				<span>213-482-9209</span>  |   <span><a href="mailto:info@thepark1400sunset.com">thepark1400sunset@gmail.com </a></span></p>
 
 				<div class="social-icons">
-					<a href="https://www.facebook.com/pages/The-Park-Restaurant/104494590265"><img src="http://www.thepark1400sunset.com/img/facebook.png" width="25" height="26" alt="Facebook logo" title="Become a fan of The Park Restaurant on Facebook" class="facebook-icon"></a><a href="https://twitter.com/parkrestaurant"><img src="http://www.thepark1400sunset.com/img/twitter.png"  width="29" height="24" alt="Twitter logo" title="Follow The Park on Twitter"></a>
+					<a href="https://www.facebook.com/pages/The-Park-Restaurant/104494590265"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/facebook.png" width="25" height="26" alt="Facebook logo" title="Become a fan of The Park Restaurant on Facebook" class="facebook-icon"></a><a href="https://twitter.com/parkrestaurant"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/twitter.png"  width="29" height="24" alt="Twitter logo" title="Follow The Park on Twitter"></a>
 				</div>
 
 			</div>
@@ -20,8 +20,11 @@
 				<h3>Hours</h3>
 
 				<p>Dinner<br>
-				<span>Mon/Wed/Thur/Sat/Sun:  5:30 - 10:00<br>
-				Tues/Fri:  6:00-10:00</span></p>
+				<span>Mon-Sun:  5:30 - 10:00<br>
+				</p>
+
+				<p>Happy Hour<br>
+				<span>Mon-Fri:  2:30 - 5:30</span></p>
 
 				<p>Lunch<br>
 				<span>Mon - Fri:  11:00 - 2:30</span></p>
@@ -39,7 +42,7 @@
 				<div id="mc_embed_signup">
 
 					<form action="http://thepark1400sunset.us4.list-manage.com/subscribe/post?u=efba98615d22d2979f4a24db6&amp;id=2d60dd07b7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					
+
 						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email@yourdomain.com" required>
 
 						<p><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></p>
